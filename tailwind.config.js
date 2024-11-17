9e346b7b929c753b30e43761b1a2b452e37608dd
Sarah <sarahcbeirigo@gmail.com>
2024-11-17 11:21:16 -0300

    Variaveis Tailwind
    Adicionando variaveis taillwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        azulfundo: '#041218',
+        azulprimario: '#2839AB',
+        azulsecundario: '#9CA7E5',
+        azultres: '#162585',
+        white: '#EFEFEF',
+      },
+      fontSize: {
+        '3xl': '1.75rem',
+      },
+      backgroundImage: {
+        'bg-banner': "url('')",
+      },
+    },
   },
   plugins: [],
-};
+}
